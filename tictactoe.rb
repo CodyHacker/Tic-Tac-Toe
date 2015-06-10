2607d6353d5441e74f39e8f2f4dbe455ffd7d871
Cody Hacker <jbalsamo@cox.net>
2015-06-10 16:50:55 -0700

    encapsulating data

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,14 +1,38 @@
 class GameBoard
   def initialize
-    
+    @@tic_tac_toe_board = Array.new(3) { Array.new(3) }
   end
+
+  def self.tic_tac_toe_board
+    @@tic_tac_toe_board
+  end
+
+  def put_piece
+    @@tic_tac_toe_board[@row][@column] = @x_or_o
+  end
+
+  def make_move(row, column, x_or_o)
+    @row = row
+    @column = column
+    @x_or_o = x_or_o
+
+    if @@tic_tac_toe_board.is_empty?
+      put_piece(@row, @column, @x_or_o)
+    else
+      warn_user
+    end
+
+    def is_empty?
+      
+    end
 end
 
 
-tic_tac_toe_board = Array.new(3) { Array.new(3) }
+my_game = GameBoard.new
 
-tic_tac_toe_board[0][0] = "X"
-tic_tac_toe_board[2][2] = "O"
-tic_tac_toe_board[1][1] = "O"
+my_game.make_move(1,1,"X")
 
-p tic_tac_toe_board
+# tic_tac_toe_board[2][2] = "O"
+# tic_tac_toe_board[1][1] = "O"
+
+p GameBoard.tic_tac_toe_board
