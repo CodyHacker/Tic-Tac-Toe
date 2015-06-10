a4864e148518af7279a8f315202e6516fd2fad5c
Cody Hacker <jbalsamo@cox.net>
2015-06-10 14:26:47 -0700

    created class and array

---
+++ tictactoe.rb
@@ -0,0 +1,14 @@
+class GameBoard
+  def initialize
+    
+  end
+end
+
+
+tic_tac_toe_board = Array.new(3) { Array.new(3) }
+
+tic_tac_toe_board[0][0] = "X"
+tic_tac_toe_board[2][2] = "O"
+tic_tac_toe_board[1][1] = "O"
+
+p tic_tac_toe_board
