9e1b055840512fdfd036ac5a0a75efd7a592e972
Cody Hacker <jbalsamo@cox.net>
2015-06-11 08:27:38 -0700

    checks rows and diags

--- tictactoe.rb
+++ tictactoe.rb
@@ -4,6 +4,10 @@ class GameBoard
     @@tic_tac_toe_board = Array.new(3) { Array.new(3) }
   end
 
+  def self.show_array #for debugging only
+    @@tic_tac_toe_board
+  end
+
   def self.display_board
     @@tic_tac_toe_board.each do|row| 
       row.each {|cell| print ' ' + (cell ? cell : '_') + ' '}
@@ -36,8 +40,18 @@ private
   end
 
   def three_in_line(x_or_o)
-    true
+    if @@tic_tac_toe_board.flatten.first == x_or_o && @@tic_tac_toe_board.flatten[4] == x_or_o && @@tic_tac_toe_board.flatten.last == x_or_o ||
+      @@tic_tac_toe_board.flatten[2] == x_or_o && @@tic_tac_toe_board.flatten[4] == x_or_o && @@tic_tac_toe_board.flatten[6] == x_or_o      
+      true
+    end
+    @@tic_tac_toe_board.each do |row|
+      if row.all? {|cell| cell == x_or_o}
+        return true
+      end
+    end
+    false
   end
+
 end
 
 
@@ -48,18 +62,14 @@ my_game = GameBoard.new
 GameBoard.display_board
 puts
 
-
-
+my_game.make_move(0,0,"O")
 my_game.make_move(0,1,"X")
-
-my_game.make_move(1,1,"X")
-my_game.make_move(2,1,"O")
+my_game.make_move(0,2,"X")
 
 
-my_game.make_move(0,0,"X")
-my_game.make_move(2,2,"X")
+
+
 GameBoard.display_board
-
 my_game.check_if_won("X")
 
 
