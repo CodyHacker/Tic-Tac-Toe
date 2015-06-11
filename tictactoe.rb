079051e7a5e97e3e402b2da3446ff865c5e6d629
Cody Hacker <jbalsamo@cox.net>
2015-06-11 07:39:52 -0700

    start of check_if_won

--- tictactoe.rb
+++ tictactoe.rb
@@ -4,7 +4,6 @@ class GameBoard
     @@tic_tac_toe_board = Array.new(3) { Array.new(3) }
   end
 
-
   def self.display_board
     @@tic_tac_toe_board.each do|row| 
       row.each {|cell| print ' ' + (cell ? cell : '_') + ' '}
@@ -12,6 +11,12 @@ class GameBoard
     end
   end
 
+  def check_if_won(x_or_o)
+    if three_in_line(x_or_o)
+      puts x_or_o + " wins!"
+      exit
+    end
+  end
 
   def make_move(row, column, x_or_o)
     @row = row
@@ -29,7 +34,13 @@ private
   def warn_user
     puts "That square is taken!"
   end
+
+  def three_in_line(x_or_o)
+    true
+  end
 end
+
+
 
 
 my_game = GameBoard.new
@@ -40,14 +51,18 @@ puts
 
 
 my_game.make_move(0,1,"X")
+
 my_game.make_move(1,1,"X")
 my_game.make_move(2,1,"O")
 
 
-my_game.make_move(0,0,"O")
+my_game.make_move(0,0,"X")
 my_game.make_move(2,2,"X")
-
 GameBoard.display_board
+
+my_game.check_if_won("X")
+
+
 
 
 # tic_tac_toe_board[2][2] = "O"
